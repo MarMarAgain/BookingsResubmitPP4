0169dca1e5437890db3f0944f30a80a182fcf81c
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2024-10-09 15:47:34 +0100

    Config vars moved/ added to Heroku

--- booking/urls.py
+++ booking/urls.py
@@ -3,13 +3,11 @@
 from django.urls import path
 from .views import book_studio,booking_confirmation, studio_list, studio_detail
 from django.urls import path
-from .views import send_test_email
 
 
 urlpatterns = [
     path('studios/', studio_list, name='studio_list'),  # Accessible at /booking/studios/
     path('studios/<int:studio_id>/', studio_detail, name='studio_detail'),  # Accessible at /booking/studios/<studio_id>/
     path('studios/<int:studio_id>/book/', book_studio, name='book_studio'),  # Accessible at /booking/studios/<studio_id>/book/
-    path('confirmation/<int:booking_id>/', booking_confirmation, name='booking_confirmation'),  # Accessible at /booking/confirmation/<booking_id>/
-    path('send-test-email/', send_test_email, name='send_test_email'),
+    path('confirmation/<int:booking_id>/', booking_confirmation, name='booking_confirmation'),  # Accessible at /booking/confirmation/<booking_id>/,
 ]
