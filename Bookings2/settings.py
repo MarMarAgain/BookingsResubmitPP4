8a1a475bde3bde3b03573232d7ca7de7888fdd97
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2024-10-09 16:06:14 +0100

    Updated settings for static files and email configuration

--- Bookings2/settings.py
+++ Bookings2/settings.py
@@ -127,7 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
